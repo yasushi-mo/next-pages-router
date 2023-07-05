0f1bbf63e70a2efb254028a116a93c6457edaae3
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2023-07-05 09:03:49 +0900

    Add:SSG

--- src/pages/static-site-generation.tsx
+++ src/pages/static-site-generation.tsx
@@ -1,9 +1,16 @@
 import Head from "next/head";
 import { title } from "process";
+import { SAMPLE_API_ENDPOINT } from "./api";
+import { RandomJoke } from "@/types";
+import { FetchedRandomJoke } from "@/components/FetchedRandomJoke";
 
 export const TITLE = "Static-Site Generation (SSG)";
 
-export default function StaticSiteGeneration() {
+export default function StaticSiteGeneration({
+  randomJoke,
+}: {
+  randomJoke: RandomJoke;
+}) {
   return (
     <>
       <Head>
@@ -11,9 +18,24 @@ export default function StaticSiteGeneration() {
       </Head>
       <h1>{TITLE}</h1>
       <p>
-        Data fetched at build-time on the server-side before sending to the
-        client.
+        Data fetched at <b>build-time</b> on the server-side before sending to
+        the client.
       </p>
+      <FetchedRandomJoke randomJoke={randomJoke} />
     </>
   );
+}
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries.
+export async function getStaticProps() {
+  const response = await fetch(SAMPLE_API_ENDPOINT);
+  const randomJoke = await response.json();
+
+  return {
+    props: {
+      randomJoke, // will be passed to the page component as props
+    },
+  };
 }
